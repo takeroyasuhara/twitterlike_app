6a8a35dcb0f7c4845b7a14b6a66fcaea4f3dc647
takero.yasuhara <takero.yasuhara@litalico.co.jp>
2015-08-06 08:17:25 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -35,6 +36,13 @@ class User < ActiveRecord::Base
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
 end
